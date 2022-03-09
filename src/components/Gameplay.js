54d0bdd506b0264b3431f46595361c656355f018
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-03-09 11:59:44 -0500

    fix loading indicator for image map. delay start of timer until image loads

--- src/components/Gameplay.js
+++ src/components/Gameplay.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react';
 import { followCursor } from 'tippy.js';
 import Tippy from '@tippyjs/react';
-import {ClockLoader } from 'react-spinners';
 import { ImageMap } from '@qiuz/react-image-map';
-import { css } from '@emotion/react';
 import universe_113 from '../images/universe_113.jpg';
 import the_loc_nar from '../images/the_loc_nar.jpg';
 import ultimate_space_battle from '../images/ultimate_space_battle.jpg';
+import clock from '../images/clock2.gif';
 import { universe113MapArea, onUniverse113MapClick } from './universe113Map';
 import { onTheLocNarMapClick, theLocNarMapArea } from './theLocNarMap';
 import {onUltimateSpaceBattleMapClick, ultimateSpaceBattleMapArea} from './ultimateSpaceBattleMap';
@@ -21,41 +20,63 @@ import 'react-toastify/dist/ReactToastify.css';
 import './cmptStyles/stylesToOverrideDefaultToastStyles.css';
 
 
-const { gameplayParentDiv, imageMap, ultimateImageMap, imageMapDefault } = styles;
+const { gameplayParentDiv, imageMap, ultimateImageMap, imageMapDefault, loadingClock } = styles;
 
 const Gameplay = ({ map, timeElapsed, setTimeElapsed, setGameOver }) => {
+
+    const {mapName, characters: chars} = map;
+    let [charsFound, setCharsFound] = useState([]);
+    let [targetCharClicked, setTargetCharClicked] = useState(false);
+    let [tippyDropdownInstance, setTippyDropdownInstance] = useState(null);
+    let [imgMapSrc, setImgMapSrc] = useState(clock);
+    let [imgMapClass, setImgMapClass] = useState(loadingClock);
 
     useEffect(() => {
         const timerInterval = setInterval(() => {
                 setTimeElapsed(timeElapsed => timeElapsed + 1);
             }, 1000);    
         return () => clearInterval(timerInterval);
-    }, []);
+    }, [imgMapSrc]);
+    // the timer will only start to count once the map image has painted to the screen.  otherwise, 
+    // it would be counting while the player would be waiting for it to load
 
-    const {mapName, characters: chars} = map;
-    let [charsFound, setCharsFound] = useState([]);
-    let [targetCharClicked, setTargetCharClicked] = useState(false);
-    let [tippyDropdownInstance, setTippyDropdownInstance] = useState(null);
-
-    const loaderCSS = css` display: block; position: fixed; top: 50vh; left: 50vw;`;
-
-    let mapImage;
+    let mapImage = new Image();  // the <img /> element is created but it itself is not rendered to 
+    // the DOM.  It is created soley because when the image is done loading, it will fire the onload
+    // event, at which time the ImageMap's src attribute can be set to the mapImages's src. Until 
+    // then, ImageMap will display a loading image.  Unfortunately this functionality (the onload 
+    // event listening) cannot implented via the ImageMap alone
     let mapArea;
     let mapClickHandler;
     // eslint-disable-next-line default-case
     switch (mapName) {
         case 'Universe 113':
-            mapImage = universe_113;
+            mapImage.src = universe_113;
+            mapImage.onload = () => {
+                setImgMapSrc(universe_113);
+                setImgMapClass(imageMapDefault);
+            }
             mapArea = universe113MapArea;
             mapClickHandler = onUniverse113MapClick;
             break;
         case 'The Loc Nar':
-            mapImage = the_loc_nar;
+            mapImage.src = the_loc_nar;
+            mapImage.onload = () => {
+                setImgMapSrc(the_loc_nar);
+                setImgMapClass(imageMapDefault);
+            }
             mapArea = theLocNarMapArea;
             mapClickHandler = onTheLocNarMapClick;
             break;
         case 'Ultimate Space Battle':
-            mapImage = ultimate_space_battle;
+            mapImage.src = ultimate_space_battle;
+            mapImage.onload = () => {
+                setImgMapSrc(ultimate_space_battle);
+                setImgMapClass(ultimateImageMap);
+                // the Ultimate Space Battle map is much wider than the other maps.  The 
+                // 'ultimateImageMap' class is applied only to this map in order to have it appear 
+                // "zoomed-in" enough for the player to be able to identify and click on the 
+                // characters
+            }
             mapArea = ultimateSpaceBattleMapArea;
             mapClickHandler = onUltimateSpaceBattleMapClick;
             break;
@@ -94,21 +115,12 @@ const Gameplay = ({ map, timeElapsed, setTimeElapsed, setGameOver }) => {
             forward ref.  Since <ImageMap> comes from a library and unable to do so, workaround is 
             wrapping with the <span>  */}
                 <span tabIndex='0' className={imageMap}>
-                    {/* since the clockLoader will get covered up by the imageMap once it finally 
-                    paints to the screen, it doesn't need to be programatically removed, so the 
-                    loading prop is set to a static true */}
-                    <ClockLoader loading={true} css={loaderCSS} color='#FFFFFF' />
                     <ImageMap
-                    src={mapImage}
+                    src={imgMapSrc}
                     map={mapArea}
                     onMapClick={(area, index) => mapClickHandler(setTargetCharClicked, area, index)}
                     onClick={() => setTargetCharClicked(false)}
-                    className={mapName === 'Ultimate Space Battle' ? ultimateImageMap : imageMapDefault}
-                    // the Ultimate Space Battle map is much wider than the 
-                    // other maps.  The 'ultimateImageMap' class is applied only 
-                    // to this map in order to have it appear "zoomed-in" enough
-                    // for the player to be able to identify and click on the 
-                    // characters
+                    className={imgMapClass}
                     />
                 </span>
             </Tippy>
